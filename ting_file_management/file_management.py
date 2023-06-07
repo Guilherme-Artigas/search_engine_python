6b32e815f43871329d07d5082a4a87764ac2b401
Guilherme Artigas <guilherme.artigas92@gmail.com>
2023-06-07 13:53:43 -0300

    feat: implementação da função txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,28 +1,19 @@
+import sys
+
+
 def txt_importer(path_file):
     if not str(path_file).endswith(".txt"):
-        return print("Formato inválido")
+        return print("Formato inválido", file=sys.stderr)
 
     line_list = []
 
     try:
         with open(path_file) as file:
             for line in file:
-                line_list.append(line)
-        print(line_list)
+                line_list.append(line.strip())
         return line_list
     except Exception:
-        return print(f"Arquivo {path_file} não encontrado")
-
-    # 2.1 - Será validado que o método txt_importer deve retornar uma lista
-    # contendo as linhas do arquivo
-
-    # 2.2 - Será validado que ao executar o método txt_importer com um arquivo
-    # TXT que não exista, deve ser exibida a mensagem Arquivo {path_file} não
-    # encontrado na stderr, em que {path_file} é o caminho do arquivo
-
-    # 2.3 - Será validado que ao executar o método txt_importer com uma
-    # extensão diferente de .txt, deve ser exibida a mensagem Formato inválido
-    # na stderr
+        return print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
 
 
-txt_importer("ting_file_management/teste.txt")
+txt_importer("ting_file_management/teste.csv")
