52ef81c5e28339e4eeb4b1104832fdd56ea9ea04
Guilherme Artigas <guilherme.artigas92@gmail.com>
2023-06-08 16:25:46 -0300

    feat: implementação da funcionalidade search_by_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -20,4 +20,26 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    ...
+    occurrence_list = []
+    occ = {}
+    occ["palavra"] = ""
+    occ["arquivo"] = ""
+    occ["ocorrencias"] = []
+    count = 1
+
+    for i in range(0, instance._tamanho):
+        for linha in instance._queue[i]["linhas_do_arquivo"]:
+            if str(word).lower() in str(linha).lower():
+                occ["palavra"] = word
+                occ["arquivo"] = instance._queue[i]["nome_do_arquivo"]
+                occ["ocorrencias"].append(
+                    {
+                        "linha": count,
+                        "conteudo": linha
+                    }
+                )
+                if len(occurrence_list) == 0:
+                    occurrence_list.append(occ)
+            count += 1
+        count = 1
+    return occurrence_list
