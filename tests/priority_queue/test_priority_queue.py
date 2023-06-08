ce1dc53a4ce637f5463ec5fc1d1c5288298b3f8d
Guilherme Artigas <guilherme.artigas92@gmail.com>
2023-06-08 13:53:05 -0300

    tests: verificando comportamento da classe PriorityQueue

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,15 +1,22 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.file_process import process
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
-    # O teste rejeita implementações que não validam a funcionalidade de cada
-    # método
+    container = PriorityQueue()
+    process("statics/novo_paradigma_globalizado.txt", container)
+    process("statics/nome_pedro.txt", container)
+    process("statics/novo_paradigma_globalizado-min.txt", container)
+    process("statics/arquivo_teste.txt", container)
 
-    # O teste rejeita implementações que tratam todos os elementos com a mesma
-    # prioridade
+    assert len(container.high_priority) == 2
+    assert len(container.regular_priority) == 2
 
-    # O teste rejeita implementações que não levantam exceção ao acessar
-    # índices inválidos para Filas
+    container.dequeue()
 
-    # O teste aprova implementações corretas
+    file_one = container.search(0)
+    assert file_one["nome_do_arquivo"] == "statics/arquivo_teste.txt"
+
+    with pytest.raises(IndexError):
+        container.search(10)
